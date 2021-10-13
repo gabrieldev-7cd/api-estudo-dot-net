e147c1d283cede2c166a6ad2a8e696f673feb288
gabriel-dev7cd <gabriel.alexandre.dev@gmail.com>
2021-10-13 09:00:14 -0300

    Criacao da camada de Exception e Validation das entidades, inclusao da lib FluentValidation

---
+++ src/Manager.Domain/Entities/User.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using Manager.Domain.Validators;
+//using Manager.Core.Exceptions;
+
+namespace Manager.Domain.Entities
+{
+    public class User : Base
+    {
+        
+        public string Name { get; private set; }        
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+        
+        //EF Construct
+        protected User(){}
+
+        public User(string name, string email, string password)
+        {
+            Name = name;
+            Email = email;
+            Password = password;
+            _errors = new List<string>();
+        }
+
+        public void ChangeName(string name)
+        {
+            Name = name;
+            Validate();
+        }
+
+        public void ChangePassword(string password)
+        {
+            Email = Email;
+            Validate();
+        }
+
+        public void ChangeEmail(string email)
+        {
+            Email = email;
+            Validate();
+        }
+
+        public override bool Validate()
+        {
+            var validator = new UserValidator();
+            var validation = validator.Validate(this);
+
+            if(!validation.IsValid)
+            {
+                foreach(var error in validation.Errors)
+                {
+                    _errors.Add(error.ErrorMessage);
+                }
+                throw new Exception("Alguns campos então inválidos, por favor corrija-os!" + _errors[0]);
+            }
+
+            return true;
+        }
+    }
+}
